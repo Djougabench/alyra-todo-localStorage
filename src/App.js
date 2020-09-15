73944936d0ca1c2812b38233fe0c3cdad6b7dbd3
Miklsan <mborghmans@gmail.com>
2020-09-15 17:45:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from "react"
+import ModeSwitch from "./components/ModeSwitch"
 import Todos from "./components/Todos"
-
+import ModeContextProvider from "./context/ModeContext"
+import AppContainer from "./components/AppContainer"
 function App() {
   return (
-    <div className="container my-4">
-      <h1 className="text-center">ToDos App</h1>
-      <Todos />
-      <p className="mt-5">
-        Icons made by{" "}
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-          Freepik
+    <ModeContextProvider>
+      <AppContainer>
+
+        <div className="container my-4">
+          <h1 className="text-center">ToDos App</h1>
+          <ModeSwitch />
+          <Todos />
+
+          <p className="mt-5">
+            Icons made by{" "}
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
         </a>{" "}
         from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          {" "}
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              {" "}
           www.flaticon.com
         </a>
-      </p>
-    </div>
+          </p>
+        </div>
+      </AppContainer>
+    </ModeContextProvider>
   )
 }
 
